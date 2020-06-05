f4944d551a0f7c93d2282496d4fec42c63b3cd3a
Daniel K <fredy.c@seznam.cz>
2020-06-05 19:56:30 +0200

    Initial commit

---
+++ cra/README.md
@@ -0,0 +1,5 @@
+# Reproduction of issue
+
+1. Install deps with Yarn (from root)
+2. Run `yarn test` (from root)
+3. Change `outside/getName.js`
---
+++ cra/src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { getName } from "~outside/getName";
+
+function App() {
+  return <div>{getName()}</div>;
+}
+
+export default App;
